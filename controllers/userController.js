015e994789e751f3a95bfcbd21ad6a167aceaf2c
Adarsh <pandey.adarshking@gmail.com>
2023-08-02 23:20:05 +0530

    error- login functionality is not working working properly

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,6 @@
 const userModel = require("../models/userModel");
-
-// get all users
-
-exports.getAllUsers = () => { };
-
-
-// create user register user 
+const bcrypt = require("bcrypt");
+//create user register user
 exports.registerController = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -24,10 +19,10 @@ exports.registerController = async (req, res) => {
                 message: "user already exisits",
             });
         }
-
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         //save new user
-        const user = new userModel({ username, email, password });
+        const user = new userModel({ username, email, password: hashedPassword });
         await user.save();
         return res.status(201).send({
             success: true,
@@ -44,5 +39,67 @@ exports.registerController = async (req, res) => {
     }
 };
 
+// get all users
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await userModel.find({});
+        return res.status(200).send({
+            userCount: users.length,
+            success: true,
+            message: "all users data",
+            users,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message: "Error In Get ALl Users",
+            error,
+        });
+    }
+};
+
 //login
-exports.loginController = () => { };
+//login
+exports.loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        //validation
+        if (!email || !password) {
+            return res.status(401).send({
+                success: false,
+                message: "Please provide email or password",
+            });
+        }
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(200).send({
+                success: false,
+                message: "email is not registerd",
+            });
+        }
+        //password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).send({
+                success: false,
+                message: "Invlid username or password",
+            });
+        }
+        return res.status(200).send({
+            success: true,
+            messgae: "login successfully",
+            user,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message: "Error In Login Callcback",
+            error,
+        });
+    }
+};
+
+
+// check login functionality something is wrong
